8507b8f1645203f8e94ccb7090552ab342cfd485
Charlotte C. Brown <charlotte@atssoftware.com>
2024-05-11 19:53:06 -0400

    Good lord it's finally working

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const coolAscii = require('cool-ascii-faces');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 const PORT = process.env.PORT || 5001
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/ahoy', (req, res) => res.send(coolAscii()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/ahoy', (req, res) => res.send(coolAscii()))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient =>
+{
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
