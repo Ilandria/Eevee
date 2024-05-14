d7cb78b974d021af3e44e84d1c2732f8242127d2
Charlotte C. Brown <charlotte@atssoftware.com>
2024-05-14 13:39:49 -0400

    Memes?

--- main.ts
+++ main.ts
@@ -29,10 +29,8 @@ container.add('eve', eve);
 // Express config.
 const ex = express();
 ex.get('/', (req, res) => res.send(`Hoooi! ${coolAscii()}`)); // Todo: Remove this test code.
+ex.get('/adrenamite', (req, res) => res.return("./pages/adrenamite.html"));
 ex.get('/api/item-groups', async (request, response) => response.send(JSON.stringify(await eve.execute("item-groups"))));
+ex.get('/api/market-prices', async (request, response) => response.send(JSON.stringify(await eve.execute("market-prices"))));
 ex.listen(process.env.PORT, () => console.log(`EXPRESS | Listening on ${process.env.PORT}`));
 container.add('express', ex);
-
-// Delete this.
-//const marketPrices = await eve.execute('market-prices');
-//console.log(JSON.stringify(marketPrices));
