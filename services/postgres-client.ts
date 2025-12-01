dcd676bf0624462e69d4c0c9a13e78424b0c9ce3
Charlotte C. Brown <charlotte@atssoftware.com>
2025-12-01 17:21:02 -0500

    How about this?

--- services/postgres-client.ts
+++ services/postgres-client.ts
@@ -9,7 +9,7 @@ export default class PostgresClient
 
 	public constructor()
 	{
-		this.client = postgres("postgres://u366esdu7hb2qq:p201b0f8b3e77fd0d8586bddbe7a69ffa1dd58c9d678f0e61d78c99de3daae091@c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d1g2d3ik5bc2jk");
+		this.client = postgres(null, { ssl : { require: true, rejectUnauthorized: false }});
 	}
 
 	public async query(query: string): Promise<object>
