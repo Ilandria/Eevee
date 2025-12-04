bb9613763ce3b072759f8af0f4f68a9ae8be62fc
Charlotte C. Brown <charlotte@atssoftware.com>
2025-12-04 15:09:46 -0500

    Trying to make name bold.

--- discord/commands/chronicle/chronicle-generate-card-command.ts
+++ discord/commands/chronicle/chronicle-generate-card-command.ts
@@ -132,8 +132,8 @@ export default class ChronicleGenerateCardCommand extends DiscordCommand
 		context.textBaseline = "middle";
 
 		// Card name.
-		let fontSize = 150;
-		context.font = `${fontSize}px Garamond`;
+		let fontSize = 130;
+		context.font = `normal 900 ${fontSize}px Garamond`;
 		context.fillText(card.name, canvas.width / 2, 110, 900);
 
 		// Card stat setup.
@@ -158,17 +158,17 @@ export default class ChronicleGenerateCardCommand extends DiscordCommand
 
 		// Collection.
 		context.textAlign = "left";
-		context.fillText(`${(card.rarity as ChronicleRarity).toString().at(0)} ${card.setCode} ${card.setNumber}`, 337.5, canvas.height - 112.5);
+		context.fillText(`${(card.rarity as ChronicleRarity).toString().at(0)} ${card.setCode} ${card.setNumber}`, 337.5, canvas.height - 112.5, 225);
 
 		// Artist.
-		context.fillText(card.artist, 337.5, canvas.height - 75);
+		context.fillText(card.artist, 337.5, canvas.height - 75, 225);
 
 		// Copyright.
 		context.textAlign = "right";
-		context.fillText(`©${card.copyright}`, canvas.width - 337.5, canvas.height - 112.5);
+		context.fillText(`©${card.copyright}`, canvas.width - 337.5, canvas.height - 112.5, 225);
 
 		// Creator.
-		context.fillText("Charlotte Brown", canvas.width - 337.5, canvas.height - 75);
+		context.fillText("Charlotte Brown", canvas.width - 337.5, canvas.height - 75, 225);
 
 		// Subtypes.
 		fontSize = 60;
